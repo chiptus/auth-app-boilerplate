b69c7e63f2b257fbd4bfdd3e9ca0f476bf2ec425
chiptus <chiptus@gmail.com>
2017-03-22 12:55:59 +0200

    auth module

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 
-const authRouter = require('./auth');
+const { router: authRouter } = require('../auth');
 
 const router = new express.Router();
 
