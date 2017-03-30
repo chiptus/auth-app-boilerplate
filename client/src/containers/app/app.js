9c9721e740268027514261a1b12bf3b7c14ead7e
chiptus <chiptus@gmail.com>
2017-03-30 17:57:40 +0300

    use layout and router

--- client/src/containers/app/app.js
+++ client/src/containers/app/app.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
+import { Header, ContentContainer, Footer } from '../../components/layout';
+import Router from '../../components/router';
 
 class App extends Component {
   render() {
     return (
       <div className="app">
-        <div className="app-header">
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="app-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Header />
+        <ContentContainer>
+          <Router />
+        </ContentContainer>
+        <Footer />
       </div>
     );
   }
