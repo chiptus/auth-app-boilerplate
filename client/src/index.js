93b66f401d837a59d14200a0675dcd57e5a57b7f
chiptus <chiptus@gmail.com>
2017-03-30 17:55:19 +0300

    use font-awesome

--- client/src/index.js
+++ client/src/index.js
@@ -2,5 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './containers/app';
 import './index.css';
+import 'font-awesome/css/font-awesome.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
