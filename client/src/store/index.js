cccc7bc2b1f6247a54c04fa8797e864ec4a0be20
chiptus <chiptus@gmail.com>
2017-04-20 08:23:00 +0300

    redux thunk middleware

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,5 +1,6 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 
 import reducer from '../reducers';
+import thunk from 'redux-thunk';
 
-export default createStore(reducer);
+export default createStore(reducer, applyMiddleware(thunk));
