49ef1ee42c5ec0467e43e7e06f6aac1453793fe2
chiptus <chiptus@gmail.com>
2017-04-20 07:43:30 +0300

    login page receives login action

--- client/src/components/login-page/login-page.js
+++ client/src/components/login-page/login-page.js
@@ -1,15 +1,24 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './login-page.css';
 
 import LoginButton from './login-button';
 
-const LoginPage = () => {
+const LoginPage = ({ login }) => {
   return (
     <div className="login-page">
-      <LoginButton socialNetwork="facebook">Login with facebook</LoginButton>
-      <LoginButton socialNetwork="twitter">Login with twitter</LoginButton>
+      <LoginButton socialNetwork="facebook" login={login}>
+        Login with facebook
+      </LoginButton>
+      <LoginButton socialNetwork="twitter" login={login}>
+        Login with twitter
+      </LoginButton>
     </div>
   );
+};
+
+LoginPage.propTypes = {
+  login: PropTypes.func.isRequired,
 };
 
 export default LoginPage;
