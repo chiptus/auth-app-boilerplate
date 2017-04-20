616cc0b1cf34fffedd6dc4c3b031dbc0f20daf57
chiptus <chiptus@gmail.com>
2017-04-20 09:06:28 +0300

    login to facebook

--- client/src/actions-creators/auth.js
+++ client/src/actions-creators/auth.js
@@ -1,3 +1,54 @@
-export function login() {}
+import hello from 'hellojs';
+import { AUTH } from '../constants/actions';
+import { FACEBOOK_APP_ID, TWITTER_APP_ID } from '../config/social-networks';
 
-export function logout() {}
+hello.init({
+  facebook: FACEBOOK_APP_ID,
+  twitter: TWITTER_APP_ID,
+});
+
+export function login(sn) {
+  return dispatch => {
+    return hello(sn)
+      .login({ scope: 'basic,email' })
+      .then(
+        function({ authResponse }) {
+          return authResponse.access_token;
+        },
+        reason => {
+          console.error(reason);
+          dispatch(reason);
+        }
+      )
+      .then(user => {
+        dispatch(loginApproved(user));
+      });
+  };
+}
+
+export function loginApproved(user) {
+  return {
+    type: AUTH.LOGIN_APPROVED,
+    payload: { user },
+  };
+}
+
+export function requestLogin() {
+  return {
+    type: AUTH.LOGIN_REQUESTED,
+  };
+}
+
+export function failedLogin(error) {
+  return {
+    type: AUTH.LOGIN_FAILED,
+    error: true,
+    payload: new Error(error),
+  };
+}
+
+export function logout() {
+  return {
+    type: AUTH.LOGOUT,
+  };
+}
--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -1,3 +1,14 @@
+import { AUTH } from '../constants/actions';
+
 export default function(state = {}, action) {
-  return state;
+  switch (action.type) {
+    case AUTH.LOGOUT:
+      return {};
+    case AUTH.LOGIN_APPROVED:
+      return {
+        token: action.payload.user,
+      };
+    default:
+      return state;
+  }
 }
