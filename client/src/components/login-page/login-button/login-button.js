9f83b0c3da575630641efa763c99a164b0057760
chiptus <chiptus@gmail.com>
2017-04-20 07:43:43 +0300

    receive prop login button

--- client/src/components/login-page/login-button/login-button.js
+++ client/src/components/login-page/login-button/login-button.js
@@ -1,9 +1,12 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
+
 import './login-button.css';
-const LoginButton = ({ children, socialNetwork }) => {
+
+const LoginButton = ({ children, socialNetwork, login }) => {
   return (
     <div className="login-button-wrapper">
-      <button className="login-button">
+      <button className="login-button" onClick={() => login(socialNetwork)}>
         <i className={`fa fa-${socialNetwork}`} />{children}
       </button>
     </div>
