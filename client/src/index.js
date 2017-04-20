f5fe9357663dd534ccd55087198b92a7adfed6d5
chiptus <chiptus@gmail.com>
2017-04-20 07:09:22 +0300

    basic redux

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './containers/app';
-import './index.css';
+
+import { Provider } from 'react-redux';
+
+import store from './store';
+
 import 'font-awesome/css/font-awesome.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from './containers/app';
+import './index.css';
+
+ReactDOM.render(
+  <Provider store={store}><App /></Provider>,
+  document.getElementById('root')
+);
